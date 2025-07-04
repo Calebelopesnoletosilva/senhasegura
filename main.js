84db8f32e0c98a6317ffbf9854f252440948b810
Calebelopesnoletosilva <lopesscalebe858@gmail.com>
2025-07-03 21:46:21 -0300

    ok

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 const numerosenha = document.querySelector('.paramenrtro-senha_texto');
 let tamanhoSenha = 12;
-numerosSnha.texContent =
+numerosSnha.texContent = tamanhoSenha;
+const letraMaisculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letraMaisculas = 'abcdefghijklmnoprstuvwyz';
+const numeros = '0123456789';
+const simbolos = '!@%?';
+const botoes = document.querySelector('.paranmetro-senha__botao');
+const campoSenha = document.querySelectorAll('#campo-senha');
+const  checkbox = document.querySelector('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho
+
+function diminuiTamanho(){
+    if(tamanhoSenha >1){
+        tamanhoSenha--;
+}
+numerosenha.textContent = tamanhoSenha;
+geraSenha();
+}
+
+function aumentaTamanho(){
+    if(tamanhoSenha <20){
+        tamanhoSenha++;
+}
+numerosenha.textContent = tamanhoSenha;
+geraSenha();
+}
